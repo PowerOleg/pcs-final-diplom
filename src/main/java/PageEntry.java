4774a96d4878a88a64d2b9e8c0d5241b37e2d1ff
PowerOleg <geraskin.oleg@mail.ru>
2023-02-06 01:13:47 +0500

    PageEntry is done. 1.1-snapshot

--- src/main/java/PageEntry.java
+++ src/main/java/PageEntry.java
@@ -3,5 +3,26 @@ public class PageEntry implements Comparable<PageEntry> {
     private final int page;
     private final int count;
 
-    // ???
+    public PageEntry(String pdfName, int page, int count) {
+        this.pdfName = pdfName;
+        this.page = page;
+        this.count = count;
+    }
+
+    @Override
+    public int compareTo(PageEntry o) {
+        return this.count - o.count;
+    }
+
+    @Override
+    public String toString() {
+        return "PageEntry{" +
+                "pdf='" + pdfName + '\'' +
+                ", page=" + page +
+                ", count=" + count +
+                '}';
+    }
+// списки ответов для каждого слова должны быть отсортированы в порядке уменьшения поля count.
+    //    класс должен выдать:
+//    PageEntry{pdf=Этапы оценки проекта_ понятия, методы и полезные инструменты.pdf, page=12, count=6}
 }
