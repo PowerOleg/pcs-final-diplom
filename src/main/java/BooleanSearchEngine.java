42520216866c6fa57b6fae274fb9cf8925f8d0cf
PowerOleg <geraskin.oleg@mail.ru>
2023-02-07 15:35:55 +0500

    BooleanSearchEngine. Constructor. 1.3-snapshot

--- src/main/java/BooleanSearchEngine.java
+++ src/main/java/BooleanSearchEngine.java
@@ -1,11 +1,16 @@
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfPage;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
 public class BooleanSearchEngine implements SearchEngine {
-    public  List<PageEntry> pageEntryList = new ArrayList<>(); //можно сделать var
+    private List<Map<String, Integer>> wordListPerPage = new ArrayList<>(); //можно сделать var
 
-    public Map<String, List<PageEntry>> resultList = new HashMap<>();             //можно сделать var
+    private Map<String, List<PageEntry>> resultList = new HashMap<>();             //можно сделать var
 
     //    Сканируя каждый пдф-файл вы перебираете его страницы, для каждой страницы извлекаете из неё слова и
 //    подсчитываете их количество. После подсчёта, для каждого уникального слова пдф-файла создаёте объект PageEntry
@@ -21,26 +26,6 @@ public class BooleanSearchEngine implements SearchEngine {
 
 
 
-
-
-
-
-
-
-
-//    для каждого уникального слова pdf(у pdf своя мапа?)
-//    Map<String, Integer> freqs = new HashMap<>(); // мапа, где ключом будет слово, а значением - частота
-//for (var word : words) { // перебираем слова
-//        if (word.isEmpty()) {
-//            continue;
-//        }
-//        word = word.toLowerCase();
-//        freqs.put(word, freqs.getOrDefault(word, 0) + 1);
-//    }
-
-
-//!Для каждой страницы нужен список слов и их количество Далее для каждого уникального слова пдф-файла создаёте объект PageEntry
-//
 
 
 //List<PageEntry> содержит список из номеров страниц pdf файла.
@@ -73,14 +58,91 @@ public class BooleanSearchEngine implements SearchEngine {
 //]
 
 
+    /* !! */   //главное - метод пробегания по pdf файлу и для каждой страницы выполняется метод добавления одной PageEntry
+//тут метод преобразования данных после индексации в результирующий лист.
+    //вычисляем кол-во страниц в документе и пробегаем
+//    public void addPageList(Map<String, Integer> freqs, String pdfName, int page) {
+//        List<Map<String, Integer>> pageEntryList1 = new ArrayList<>();
+//        //        у каждого слова свой список страниц с этим словом
+//        for (String word : freqs.keySet()) {
+//            int count = freqs.get(word);
+//            pageEntryList1.add(new PageEntry(pdfName, page, count));
+//        }
+//
+//
+//
+//
+//
+//
+//
+//
+//        pageList.add(pageEntryList1);
+//    }
 
 
+    public void addResultList(List<Map<String, Integer>> wordListPerPage) {
+        String word = "";
+//тут будет упорядочивание из листалистов слов-количеств для страниц в resultList
+
+        List<PageEntry> pageEntryList2 = new ArrayList<>();
+
+
+        resultList.put(word, pageEntryList2);
+    }
 
 
 
     public BooleanSearchEngine(File pdfsDir) throws IOException {
-        // прочтите тут все pdf и сохраните нужные данные,
-        // тк во время поиска сервер не должен уже читать файлы
+//        Spliterator<File> spliterator = Arrays.stream(pdfsDir.listFiles()).spliterator();
+//        spliterator.
+
+
+        //сделать 1
+//    public class XmlStream {
+//        static Stream<Node> of(NodeList list) {
+//            return IntStream.range(0, list.getLength()).mapToObj(list::item);
+//        }
+//    }
+
+//    сделать 2
+//    public static Stream<Integer> diff(Stream<Integer> stream) {
+//        return pairMap(stream, (a, b) -> b - a);
+//    }
+
+
+
+for (File pdf : pdfsDir.listFiles()) {
+    System.out.println("Файл " + pdf);
+
+    var doc = new PdfDocument(new PdfReader(pdf));
+    int length = doc.getNumberOfPages();
+
+    for (int i = 0; i < length; i++) {
+        PdfPage page = doc.getPage(i);
+        var text = PdfTextExtractor.getTextFromPage(page);
+        var words = text.split("\\P{IsAlphabetic}+");
+
+        Map<String, Integer> freqs = new HashMap<>(); // мапа, где ключом будет слово, а значением - частота
+        for (var word : words) { // перебираем слова
+            if (word.isEmpty()) {
+                continue;
+            }
+            word = word.toLowerCase();
+            freqs.put(word, freqs.getOrDefault(word, 0) + 1);
+            System.out.println("2 freqs " + freqs.size());
+        }
+
+        //тут метод чтобы для одной страницы положить запись
+        wordListPerPage.add(freqs);
+    }
+    System.out.println("3 wordListPerPage " + wordListPerPage.size());
+
+
+
+
+
+        }
+    addResultList(wordListPerPage);
     }
 
     @Override
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,17 +13,13 @@ public class Main {
         BooleanSearchEngine engine = new BooleanSearchEngine(new File("pdfs"));
 //        System.out.println(engine.search("бизнес"));
 
-//еще не использовал->   Полистайте методы doc чтобы найти способ узнать количество * страниц в документе.
 
 
 
 
 
-/* !! */   //главное - метод пробегания по pdf файлу и для каждой страницы выполняется метод добавления одной PageEntry
-//тут метод преобразования данных после индексации в результирующий лист.
-        //вычисляем кол-во страниц в документе и пробегаем
-        engine.pageEntryList.add(new PageEntry("1.DevOps_MLops", 1, 10)); //откуда взять аргументы?
-//        System.out.println("1 " + engine.pageEntryList);
+
+
 
 
 
@@ -31,24 +27,8 @@ public class Main {
 
 /* !! */ //    для каждой страницы одного pdf своя мапа. у меня есть то что мне нужно: имя pdf, номер страницы, count
         //это то что будет храниться после индексации
-        File pdf = new File(".\\pdfs\\1. DevOps_MLops.pdf");
-        var doc = new PdfDocument(new PdfReader(pdf));
+//        File pdf = new File(".\\pdfs\\1. DevOps_MLops.pdf");
 
-/* ! */        int page1 = 1;
-        PdfPage page = doc.getPage(page1);
-        var text = PdfTextExtractor.getTextFromPage(page);
-        var words = text.split("\\P{IsAlphabetic}+");
-
-    Map<String, Integer> freqs = new HashMap<>(); // мапа, где ключом будет слово, а значением - частота
-for (var word : words) { // перебираем слова
-        if (word.isEmpty()) {
-            continue;
-        }
-        word = word.toLowerCase();
-        freqs.put(word, freqs.getOrDefault(word, 0) + 1);
-    }
-        System.out.println("2 " + freqs.size());
-        System.out.println("3 " + freqs);
 
 
 
