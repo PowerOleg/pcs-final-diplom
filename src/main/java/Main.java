96464411c7e10fed2a83a227b22c4b31ecb8d805
PowerOleg <geraskin.oleg@mail.ru>
2023-02-08 21:07:54 +0500

    SearchServer&ServerLogic are done. 1.9-snapshot

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,9 +4,7 @@ import com.itextpdf.kernel.pdf.PdfReader;
 import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
 
 import java.io.File;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -14,6 +12,11 @@ public class Main {
 //        System.out.println(engine.search("бизнес"));
 
         Thread searchServer = new Thread(new SearchServer(engine, new ServerLogic()));
+        searchServer.start();
+//        ServerLogic serverLogic = new ServerLogic();
+//        System.out.println("1 " + serverLogic.convert(new PageEntry("pdf1", 1, 10)));
+//        String s = serverLogic.makeResponse("бизнес", engine);
+//        System.out.println("2 " + s);
         // здесь создайте сервер, который отвечал бы на нужные запросы
         // слушать он должен порт 8989
         // отвечать на запросы /{word} -> возвращённое значение метода search(word) в JSON-формате
--- src/main/java/SearchServer.java
+++ src/main/java/SearchServer.java
@@ -2,54 +2,39 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-//это response сервера
-//[
-//    {
-//        "pdfName": "Этапы оценки проекта_ понятия, методы и полезные инструменты.pdf",
-//            "page": 12,
-//            "count": 6
-//    },
-//    {
-//        "pdfName": "Этапы оценки проекта_ понятия, методы и полезные инструменты.pdf",
-//            "page": 4,
-//            "count": 3
-//    },
-//    {
-//        "pdfName": "Этапы оценки проекта_ понятия, методы и полезные инструменты.pdf",
-//            "page": 5,
-//            "count": 3
-//    }
-//]
 public class SearchServer implements Runnable {
-    private BooleanSearchEngine booleanSearchEngine;
-    private ServerLogic serverLogic;
+    public static final int PORT = 8989;
+    private final BooleanSearchEngine booleanSearchEngine;
+    private final ServerLogic serverLogic;
 
     public SearchServer(BooleanSearchEngine booleanSearchEngine, ServerLogic serverLogic) {
         this.booleanSearchEngine = booleanSearchEngine;
         this.serverLogic = serverLogic;
     }
 
-    public static final int PORT = 8989;
     @Override
     public void run() {
         try (ServerSocket serverSocket = new ServerSocket(PORT)) {
             System.out.println("Сервер запущен");
-            try (Socket socket = serverSocket.accept();
-                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))) {
-                String clientRequest = in.readLine();
-                String response = serverLogic.makeResponse(clientRequest, booleanSearchEngine);
-                out.write(response);
-                out.newLine();
-                out.flush();
+            while (true) {
+                try (Socket socket = serverSocket.accept();
+                     BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                     BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))) {
+                    String clientRequest = in.readLine();
+                    System.out.println("Запрос клиента: " + clientRequest);
+                    String response = serverLogic.makeResponse(clientRequest, booleanSearchEngine);
+                    out.write(response);
+                    out.newLine();
+                    out.flush();
+                    System.out.println("Ответ сервера: " + response);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
+
             }
-
-
         } catch (IOException e) {
-            System.out.println("Сервер не может запуститься");
             throw new RuntimeException(e);
         }
-
-
     }
 }
